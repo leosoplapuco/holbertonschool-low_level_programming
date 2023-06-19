70538bc8dabf099a9575359249e29dd17353a776
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-19 13:49:45 -0700

    Hello, pust

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - My first program with c
+ *
+ * Return: Zero on Success
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multiligual puzzle");
+return (0);
+}
