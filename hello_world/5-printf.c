1e12e2b513dbf7d962fd447e141342852f88af18
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-19 13:55:41 -0700

    Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Using printf in mi c's programs
+ *
+ * Writing in Linux but with c
+ *
+ * Return: Zero on success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
