2fed81a92a9cb2d2ce9f1c0ebb8be080183eb0f6
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-20 12:45:56 -0700

    Ten

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - only base ten
+*
+* Return: zero on success
+*/
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+printf("%i", num);
+putchar('\n');
+return (0);
+}
