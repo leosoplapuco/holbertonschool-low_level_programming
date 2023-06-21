16c4d0a59a7b20d17f8f4441ec1b5863fdcd3caa
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-21 10:32:51 -0700

    _function 7

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - _putchar
- *
- * Write a program that prints _putchar, followed by a new line.
- * 
- * Return: zero on success
- */
+*main - _putchar
+*
+*Write a program that prints _putchar, followed by a new line.
+*
+*Return: zero on success
+*/
 
-int main(void){
-
+int main(void)
+{
 char frase[] = "_putchar";
 printf("%s\n", frase);
 return (0);
-
 }
