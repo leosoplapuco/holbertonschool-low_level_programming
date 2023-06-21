dbeef8a85876682af51cda30b065b681b25cc7aa
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-21 10:36:57 -0700

    1-alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - alphabet
+*
+*Write a function that prints the alphabet, in lowercase, followed by a new line.
+*
+*Return: zero on success
+*/
+
+int main(void)
+{
+
+return (0);
+}
