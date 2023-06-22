421d842065a8d7004f8d40819b7175f6e9d2bbc2
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-22 16:45:52 -0700

    9-times_table 2

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,22 +4,22 @@
 
 /**
 * times_table - Tying numbers in rows and columns.
-* @a: column.
-* @b: row.
+* @a: index of the column.
+* @b: index of the row.
 * Return: zero on success.
 */
 
 void times_table(void)
 {
-    int a;
-    int b;
-
-    for (a = 0; a <= 9; a++)
-    {
-	    for (b = 0; b <= 9; b++)
-	    {
-		    printf("%2d ", a * b);
-	    }
-	    printf("\n");
-    }
+	int a;
+	int b;
+	
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			printf("%2d ", a * b);
+		}
+		printf("\n");
+	}
 }
