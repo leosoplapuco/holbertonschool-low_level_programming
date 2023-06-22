21d117544c1cb4209dca86bd0b04daceee40544f
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-22 11:01:29 -0700

    5-sign 17

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,19 +10,19 @@
 
 int print_sign(char n)
 {
-	if (n == 0)
-	{
-		_putchar (48);
-		return (0);
-	}
-	else if (n > 0)
+	if (n > 0)
 	{
 		_putchar (43);
 		return (1);
 	}
+	else if (n == 0)
+	{
+		_putchar (48);
+		return (0);
+	}
 	else
 	{
-		_putchar (45);
+		_putchar (43);
 		return (-1);
 	}
 	_putchar ('\n');
