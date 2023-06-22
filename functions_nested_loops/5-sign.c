477a286cef54171c2677b717998d5ba6511d931a
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-21 23:11:03 -0700

    5-sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*
+*print_singn - identifing numbers
+*
+*n: value of the number
+*
+* Return: zero on success.
+*/
+
+int print_sign(char n)
+{
+	if (n = 0)
+	{
+		putchar(48);
+		return(0);
+	}
+	else if (n > 0)
+	{
+		putchar(43);
+		return(1);
+	}
+	else
+	{
+		putchar(45);
+		return(-1);
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _isalpha(char c);
 int _islower(char c);
+int _isalpha(char c);
+int _print_sign(char n);
 int _putchar(char c);
