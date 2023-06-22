927ac2165097e77b25fc38bb9e9f797459ff067d
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-22 16:05:12 -0700

    8-24_hours 4

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,15 +10,14 @@
 void jack_bauer(void)
 {
 	int h;
-
 	int m;
 	
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
-	{
-		printf("%.2d:%.2d\n", h, m);
-	}
+		{
+			printf("%.2d:%.2d\n", h, m);
+		}
 
 	}
 }
