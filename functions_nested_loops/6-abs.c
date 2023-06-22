925aa022086659d8206ba86a9f291bf72aa31f4c
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-22 11:24:14 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _abs - Getting the absolute value.
+* @num: Value of the number.
+* Return: zero on success.
+*/
+
+int _abs(int num)
+{
+	if(num < 0)
+    {
+        num = num * -1;
+        printf("%d", num);
+    }else
+    {
+        printf("%d", num);
+    }
+}
