496128c3fafcf0d82b554b51cc9d61ea5853a72c
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-22 17:07:10 -0700

    9-times_table 8

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,20 +13,19 @@ void times_table(void)
 {
 	int a;
 	int b;
-
+	
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
 			if (b == 0)
 			{
-				printf(",");
+				printf("%d,", a * b);
 			}
 			else
 			{
-				printf(" ");
+				printf(" %d,", a * b);
 			}
-			printf("%2d", a*b);
 		}
 		printf("\n");
 	}
