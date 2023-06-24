58300fb857e7bc30731fb8e1a8ba900906119476
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-23 18:29:34 -0700

    8-print_square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_square - drawing on c.
+* @n: times that repeat the line.
+* Return: zero on success
+*/
+
+void print_square(int size)
+{
+	int row;
+	int column;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	row = 0;
+	while (row < size)
+	{
+		column = 0;
+		while (column < size)
+		{
+			_putchar('#');
+			column++;
+		}
+		_putchar('\n');
+		row++;
+	}
+}
