6567b6132414ad0a0fbeb97691dac15216555ce4
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-24 12:53:48 -0700

    9-fizz_buzz 10

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 	int i;
 
 	i = 1;
-	
+
 	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
@@ -30,6 +30,10 @@ int main(void)
 		else if (i != 0)
 		{
 			printf("%d ", i);
+		}
+		else if (i == 100)
+		{
+			printf("FizzBuzz");
 		}
 		i++;
 	}
