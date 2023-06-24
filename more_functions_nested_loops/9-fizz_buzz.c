ef15b7677c7c3d7adf88e7ac7b039cb06ddad731
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-24 14:49:27 -0700

    9-fizz_buzz 28

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -3,33 +3,40 @@
 
 /**
 * main - principal function.
-* @num: value.
+* num: value.
 * Return: zero on success
 */
 
-int main(void) {
-    int num = 1;
+int main(void)
+{
+	int num;
 
-    while (num <= 100) {
-        if (num % 3 == 0 && num % 5 == 0) {
-            printf("FizzBuzz");
-        } else if (num % 3 == 0) {
-            printf("Fizz");
-        } else if (num % 5 == 0) {
-            printf("Buzz");
-        } else {
-            printf("%d", num);
-        }
-
-        if (num != 100) {
-            printf(" ");
-        }
-
-        num++;
-    }
-
-    printf("\n");
-
-    return 0;
+	num = 1;
+	
+	while (num <= 100)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz");}
+		else
+		{
+			printf("%d", num);
+		}
+		
+		if (num != 100)
+		{
+			printf(" ");
+		}
+		num++;
+	}
+	printf("\n");
+	return (0);
 }
-
