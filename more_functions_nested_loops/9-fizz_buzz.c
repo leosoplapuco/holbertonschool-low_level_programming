4ef98f45b91fc244d554f31f5e581554735f5baf
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-24 13:23:05 -0700

    9-fizz_buzz 19

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,34 +7,43 @@
 * Return: zero on success
 */
 
-
-void fizzBuzz(int num) {
-    if (num % 3 == 0 && num % 5 == 0) {
-        printf("FizzBuzz");
-    } else if (num % 3 == 0) {
-        printf("Fizz");
-    } else if (num % 5 == 0) {
-        printf("Buzz");
-    } else {
-        printf("%d", num);
-    }
+void FizzBuzz(int num)
+{
+	if (num % 3 == 0 && num % 5 == 0)
+	{
+		printf("FizzBuzz");
+	}
+	else if (num % 3 == 0)
+	{
+		printf("Fizz");
+	}
+	else if (num % 5 == 0)
+	{
+		printf("Buzz");
+	}
+	else
+	{
+		printf("%d", num);
+	}
 }
 
-int main(void) {
-    int num = 1;
+int main(void)
+{
+	int num;
+	num = 1;
 
-    while (num <= 100) {
-        fizzBuzz(num);
-
-        if (num != 100) {
-            printf(" ");
-        }
-
-        num++;
-    }
-
-    printf("\n");
-
-    return 0;
+	while (num <= 100)
+	{
+		fizzBuzz(num);
+		
+		if (num != 100)
+		{
+			printf(" ");
+		}
+		
+		num++;
+	}
+	
+	printf("\n");
+	return (0);
 }
-
