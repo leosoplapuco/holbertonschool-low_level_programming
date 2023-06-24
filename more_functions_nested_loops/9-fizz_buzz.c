321afed24356c6d1cd057276b53ae02b3e5e25ef
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-24 14:51:39 -0700

    9-fizz_buzz 29

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 	int num;
 
 	num = 1;
-	
+
 	while (num <= 100)
 	{
 		if (num % 3 == 0 && num % 5 == 0)
@@ -25,12 +25,13 @@ int main(void)
 		}
 		else if (num % 5 == 0)
 		{
-			printf("Buzz");}
+			printf("Buzz");
+		}
 		else
 		{
 			printf("%d", num);
 		}
-		
+
 		if (num != 100)
 		{
 			printf(" ");
