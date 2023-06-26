3a93df93a1ac03b1f0b7431926ae4fee0c1b462d
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-26 13:06:59 -0700

    0-reset_to_98 3

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
-*reset_to_98 - principal function.
-*@n: value.
+/**
+* reset_to_98 - principal function.
+* @n: value.
+* Return: zero on success
 */
 
 void reset_to_98(int *n)
