8dddb346e623206798b16b725d09d6f3ae82d7fc
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-26 14:04:26 -0700

    1-swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* swap_int - principal function.
+* @a: first value.
+* @b: second value
+* Return: zero on success
+*/
+
+void swap_int(int *a, int *b)
+{}
