c626e24f43750f71efe38d2ec377cfece7829058
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-27 09:20:20 -0700

    100-atoi 2

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <limits.h>
+#include "main.h"
+
+/**
+* _atoi - principal function.
+* @s: value.
+*/
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	while (s[i] == ' ')
+	{
+		i++;
+	}
+
+	if (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+		{
+			sign = -1;
+		}
+		i++;
+	}
+	
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (result > (INT_MAX / 10) || (result == (INT_MAX / 10) && (s[i] - '0') > (INT_MAX % 10)))
+		{
+			if (sign == 1)
+			{
+				return INT_MAX;
+			}
+			else
+			{
+				return INT_MIN;
+			}
+		}
+		result = (result * 10) + (s[i] - '0');
+		i++;
+	}
+	return sign * result;
+}
