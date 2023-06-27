897c2eaebf536827a99158bd18ae96f9d14115cb
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-26 19:28:32 -0700

    1-swap 7

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strlen - principal function.
+* @s: value.
+* length: length.
+* Return: zero on success
+*/
+
+int _strlen(char *s)
+{
+	int length;
+	length = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		length++;
+	}
+	return (length);
+}
