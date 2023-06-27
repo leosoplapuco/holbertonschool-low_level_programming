27fdc3e9bc8570e9316ed128d61894173e7037b1
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-27 10:31:31 -0700

    100-atoid 8

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,26 +8,35 @@
 * @s: value.
 */
 
-int _atoi(char *s) {
-    int result = 0;
-    int sign = 1;
-    bool number_found = false;
+int _atoi(char *s)
+{
+	int result;
+	int sign;
+	bool number_found;
 
+	result = 0;
+	sign = 1;
+	number_found = false;
 
-    while (*s != '\0') {
-        if (*s == '-' || *s == '+') {
-            sign = (*s == '-') ? -1 : 1;
-        } else if (*s >= '0' && *s <= '9') {
-            result = (result * 10) + (*s - '0');
-            number_found = true;
-        } else {
-            if (number_found) {
-                break;
-            }
-        }
-
-        s++;
-    }
-
-    return (sign * result);
+	while (*s != '\0')
+	{
+		if (*s == '-' || *s == '+')
+		{
+			sign = (*s == '-') ? -1 : 1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			result = (result * 10) + (*s - '0');
+			number_found = true;
+		}
+		else
+		{
+			if (number_found)
+			{
+				break;
+			}
+		}
+		s++;
+	}
+	return (sign * result);
 }
