dfea76ad371e452e626a3f6c85ba1354aa6bb895
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-26 20:29:06 -0700

    7-puts_half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+*puts_half - Principal function.
+*@str: value.
+*Return: zero on succes.
+*/
+void puts_half(char *str)
+{
+	int length = 0;
+	char *a = str;
+	int b;
+
+	while (*a != '\0')
+	{
+		a++;
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		b = length / 2;
+	}
+	else
+	{
+		b = (length + 1) / 2;
+	}
+	for ( ; b < length ; b++)
+	{
+		_putchar(str[b]);
+	}
+
+	_putchar('\n');
+}
