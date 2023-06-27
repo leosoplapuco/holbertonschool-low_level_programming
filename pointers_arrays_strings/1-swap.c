fd7d4f504fd4ae9eedfe6f150070ef3da45b7606
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-26 18:39:11 -0700

    1-swap 2

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -8,5 +8,15 @@
 * Return: zero on success
 */
 
-void swap_int(int *a, int *b)
-{}
+void swap_int(int *a, int *b){
+	int *c;
+	int *d;
+
+	c = &a;
+	d = &b;
+
+	printf("%d\n", a, b);
+	printf("%d\n", c, d);
+
+	return (0);
+}
