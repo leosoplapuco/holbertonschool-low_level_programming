bc14177b53dc608bf620fe9ebf629f44e570a68d
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-28 11:17:51 -0700

    0-puts_recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - Principal function.
+ * @s: value;
+ * Return: zero on success.
+*/
+
+void _puts_recursion(char *s)
+{
+	printf("%s\n", *s);
+	return (0);
+}
