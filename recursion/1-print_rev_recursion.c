e1fc89e05be6f4bb6bf414d5b9df2c3d32b99cf4
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-28 16:56:27 -0700

    1-print_rev_recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _print_rev_recursion - principal function.
+*@s: value
+*Return: Zero on success.
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+}
