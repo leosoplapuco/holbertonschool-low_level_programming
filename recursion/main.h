4393f7ab340bb6331c375d22fe12601a06403860
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-28 18:51:55 -0700

    5-sqrt_recursion

--- recursion/main.h
+++ recursion/main.h
@@ -2,4 +2,6 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
 void _putchar (char c);
