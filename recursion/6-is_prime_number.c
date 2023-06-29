8220babbb05008d836dd82e638f7f85442930663
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-28 20:52:49 -0700

    6-is_prime_number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -6,3 +6,19 @@
 *@n: value-
 *Return: zero on success.
 */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return 0;
+	}
+	for (int i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
