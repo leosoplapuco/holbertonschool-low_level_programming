a47f08d2177837f131747e34bc01878f731bde0d
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-28 17:11:40 -0700

    2-strlen_recursion 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -7,10 +7,12 @@
 * Return: zero on success.
 */
 
-int _strlen_recursion(char *s){
+int _strlen_recursion(char *s)
+{
 	if (*s == '\0')
 	{
 		return (0);
 	}
+
 	return 1 + _strlen_recursion(s + 1);
 }
