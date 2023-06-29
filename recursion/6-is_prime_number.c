819ea11a6d86d59173806b00ca4af238d3915dab
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-28 21:37:55 -0700

    6-is_prime_number 7

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,32 +2,40 @@
 #include "main.h"
 
 /**
-*is_prime_number - principal function
+*is_divisible - second function
 *@n: value.
 *divisor: divisor.
 *Return: zero on success.
 */
 
-int is_divisible(int n, int divisor) {
-    if (n == 2) {
-        return 1;
-    }
-
-    if (n % divisor == 0) {
-        return 0;
-    }
-
-    if (divisor * divisor > n) {
-        return 1;
-    }
-
-    return is_divisible(n, divisor + 1);
+int is_divisible(int n, int divisor)
+{
+	if (n == 2)
+	{
+		return (1);
+	}
+	if (n % divisor == 0)
+	{
+		return (0);
+	}
+	if (divisor * divisor > n)
+	{
+		return (1);
+	}
+	return (is_divisible(n, divisor + 1));
 }
 
-int is_prime_number(int n) {
-    if (n <= 1) {
-        return 0;
-    }
+/**
+* is_prime_number - principal function.
+*@n: value
+*Return: Zero on success.
+*/
 
-    return is_divisible(n, 2);
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (is_divisible(n, 2));
 }
