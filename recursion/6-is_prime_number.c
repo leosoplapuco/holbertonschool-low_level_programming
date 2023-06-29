202df885d53df371d7e35f32539f917c31d941c0
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-28 21:22:33 -0700

    6-is_prime_number 3

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -8,24 +8,42 @@
 *Return: zero on success.
 */
 
-int is_prime_number(int n)
-{
-	int divisor;
+#include <stdio.h>
 
-	if (n <= 1)
-	{
-		return (0);
-	}
+int is_divisible(int n, int divisor) {
+    // Caso base: Si el divisor alcanza la raíz cuadrada de n, n no es divisible por ningún número y es primo
+    if (divisor * divisor > n) {
+        return 0;
+    }
 
-	divisor = 2;
+    // Si n es divisible por el divisor actual, n no es primo
+    if (n % divisor == 0) {
+        return 1;
+    }
 
-	while (divisor * divisor <= n)
-	{
-        if (n % divisor == 0)
-	{
-		return (0);
-        }
-	divisor++;
-	}
-	return (1);
+    // Llamada recursiva incrementando el divisor en 1
+    return is_divisible(n, divisor + 1);
 }
+
+int is_prime_number(int n) {
+    // Casos especiales: 0, 1 y números negativos no son primos
+    if (n <= 1) {
+        return 0;
+    }
+
+    // Llamada a la función auxiliar para verificar la divisibilidad
+    return is_divisible(n, 2);
+}
+
+int main() {
+    int num = 17;
+
+    if (is_prime_number(num)) {
+        printf("%d es un número primo.\n", num);
+    } else {
+        printf("%d no es un número primo.\n", num);
+    }
+
+    return 0;
+}
+
