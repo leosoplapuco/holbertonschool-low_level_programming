59689f07f73ef3f74abd0eb596829a8bc696666b
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-28 19:02:54 -0700

    5-sqrt_recursion 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -9,14 +9,14 @@
 
 int _sqrt_recursion(int n)
 {
+	int start;
+	int end;
+	int result;
+
 	if (n == 0 || n == 1)
 	{
 		return (n);
 	}
-
-	int start;
-	int end;
-	int result;
 
 	start = 1;
 	end = n;
