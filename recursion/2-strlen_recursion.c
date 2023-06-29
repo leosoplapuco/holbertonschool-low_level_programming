1ed68cb17ce54173a2410f92c50d6518139da1aa
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-28 17:10:45 -0700

    2-strlen_recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strlen_recursion - principal function;
+* @s: value;
+* Return: zero on success.
+*/
+
+int _strlen_recursion(char *s){
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return 1 + _strlen_recursion(s + 1);
+}
