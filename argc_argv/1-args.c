d0f7d8065aa416f8636e597ad5768d9f9be5825c
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-30 19:41:55 -0700

    1-args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - principal function.
+*@argc: count.
+*@argv: arrays.
+*Return: zero on success.
+*/
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
