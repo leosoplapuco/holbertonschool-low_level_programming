391a2bcec9861a22b43613181990e71ab55b9a9c
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-06-30 21:43:48 -0700

    4-add 3

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -10,36 +10,34 @@
 *Return: zero on success.
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i = 1;
 
-int main(int argc, char *argv[]) {
-    int sum = 0;
-    int i = 1;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    if (argc == 1) {
-        printf("0\n");
-        return 0;
-    }
+	while (i < argc)
+	{
+		char *num = argv[i];
+		int j = 0;
 
-    while (i < argc) {
-        char *num = argv[i];
-        int j = 0;
-
-        while (num[j] != '\0') {
-            if (!isdigit(num[j])) {
-                printf("Error\n");
-                return 1;
-            }
-            j++;
-        }
-
-        sum += atoi(num);
-        i++;
-    }
-
-    printf("%d\n", sum);
-    return 0;
+		while (num[j] != '\0')
+		{
+			if (!isdigit(num[j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		sum += atoi(num);
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
-
