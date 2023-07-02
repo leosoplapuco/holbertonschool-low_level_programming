b6f2e4b2c549c30bec26442764f6c2e4bca4dcc8
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-02 12:36:38 -0700

    2.str_concat 5

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,14 +8,6 @@
 *Return: zero con success.
 */
 
-#include "holberton.h"
-#include <stdlib.h>
-/**
- * *str_concat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * Return: pointer to new space in memory or null
- **/
 char *str_concat(char *s1, char *s2)
 {
 	char *strDup;
