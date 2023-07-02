52311b7055693e42ab6752e48729cd7197212ac6
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-02 12:35:33 -0700

    2.str_concat 4

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,42 +6,43 @@
 *@s1: first value.
 *@s2: second value.
 *Return: zero con success.
-**/
+*/
 
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to new space in memory or null
+ **/
 char *str_concat(char *s1, char *s2)
 {
 	char *strDup;
-	int a, b;
-
+	int i, j;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	a = b = 0;
-	while (s1[a] != '\0')
-	{
-		strDup[a] = s1[a];
-		a++;
-	}
-	while (s2[b] != '\0')
-	{
-		strDup[a] = s2[b];
-		a++, b++;
-	}
-	strDup = malloc(sizeof(char) * (a + b + 1));
+	i = j = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	strDup = malloc(sizeof(char) * (i + j + 1));
 	if (strDup == NULL)
 		return (NULL);
-	a = b = 0;
-	while (s1[a] != '\0')
+	i = j = 0;
+	while (s1[i] != '\0')
 	{
-		strDup[a] = s1[a];
-		a++;
+		strDup[i] = s1[i];
+		i++;
 	}
-	while (s2[b] != '\0')
+	while (s2[j] != '\0')
 	{
-		strDup[b] = s2[b];
-		a++, b++;
+		strDup[i] = s2[j];
+		i++, j++;
 	}
-	strDup[a] = '\0';
+	strDup[i] = '\0';
 	return (strDup);
 }
