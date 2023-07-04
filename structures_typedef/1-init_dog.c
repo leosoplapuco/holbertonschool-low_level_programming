80b8de0f9d54d66a6edd25ea533305b6e3e38f8d
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-03 20:01:10 -0700

    1-init_dog 4

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -10,13 +10,6 @@
 *Return: zero on success.
 */
 
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-}
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
