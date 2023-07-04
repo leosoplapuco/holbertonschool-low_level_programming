1f99348b18662ee2ada39aaf64209956168e9622
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-04 16:21:12 -0700

    5-free_dog 8

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,38 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-    char *name;
-    float age;
-    char *owner;
+/**
+* *new_dog - principal function
+* struct - adding information about our pets
+* @name: name.
+* @age: age.
+* @owner: owner.
+* Return: zero con success.
+*/
+
+typedef struct
+{
+	char *name;
+	float age;
+	char *owner;
 } dog_t;
 
-dog_t *new_dog(char *name, float age, char *owner) {
-    dog_t *new_dog;
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	
+	if (name == NULL || owner == NULL)
+		return NULL;
 
-    if (name == NULL || owner == NULL)
-        return NULL;
+	new_dog = malloc(sizeof(dog_t));
 
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return NULL;
+	if (new_dog == NULL)
+		return NULL;
 
-    new_dog->name = malloc(strlen(name) + 1);
-    if (new_dog->name == NULL) {
-        free(new_dog);
-        return NULL;
-    }
+	new_dog->name = malloc(strlen(name) + 1);
 
-    new_dog->owner = malloc(strlen(owner) + 1);
-    if (new_dog->owner == NULL) {
-        free(new_dog->name);
-        free(new_dog);
-        return NULL;
-    }
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return NULL;
+	}
 
-    strcpy(new_dog->name, name);
-    strcpy(new_dog->owner, owner);
-    new_dog->age = age;
+	new_dog->owner = malloc(strlen(owner) + 1);
 
-    return new_dog;
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return NULL;
+	}
+
+	strcpy(new_dog->name, name);
+	strcpy(new_dog->owner, owner);
+	new_dog->age = age;
+
+	return new_dog;
 }
