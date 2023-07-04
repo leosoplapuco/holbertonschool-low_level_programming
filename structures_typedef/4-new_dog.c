324b60be7bcfeb3a3240494e17264ecc1a9354d5
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-04 16:13:03 -0700

    5-free_dog 5

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,48 +1,30 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-/**
-* struct dog_t - adding information about our pets
-* @name: name.
-* @age: age.
-* @owner: owner.
-*/
-
-typedef struct
-{
-	char *name;
-	float age;
-	char *owner;
-}dog_t;
+#include "main.h"
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	if (name == NULL || owner == NULL)
-	{
-		return (NULL);
-		new_dog = malloc(sizeof(dog_t));
-	}
-			if (new_dog == NULL)
-			return NULL;
-			new_dog->name = malloc(strlen(name) + 1);
-			if (new_dog->name == NULL)
-			{
-				free(new_dog);
-				return NULL;
-			}
-			new_dog->owner = malloc(strlen(owner) + 1);
-			if (new_dog->owner == NULL)
-			{
-			free(new_dog->name);
-			free(new_dog);
-			return NULL;
-			}
+    dog_t *new_dog;
+    char *name_copy, *owner_copy;
 
-strcpy(new_dog->name, name);
-strcpy(new_dog->owner, owner);
-new_dog->age = age;
+    new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL)
+        return NULL;
 
-return (new_dog);
+    name_copy = strdup(name);
+    owner_copy = strdup(owner);
+    if (name_copy == NULL || owner_copy == NULL)
+    {
+        free(new_dog);
+        free(name_copy);
+        free(owner_copy);
+        return NULL;
+    }
+
+    new_dog->name = name_copy;
+    new_dog->age = age;
+    new_dog->owner = owner_copy;
+
+    return new_dog;
 }
+
