9dc9d96cb2468d98ce6ae5c585073e2e74d4bfae
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-04 16:11:02 -0700

    5-free_dog 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -19,9 +19,11 @@ typedef struct
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	if (name == NULL || owner == NULL;
-			return NULL;
-			new_dog = malloc(sizeof(dog_t));
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+		new_dog = malloc(sizeof(dog_t));
+	}
 			if (new_dog == NULL)
 			return NULL;
 			new_dog->name = malloc(strlen(name) + 1);
