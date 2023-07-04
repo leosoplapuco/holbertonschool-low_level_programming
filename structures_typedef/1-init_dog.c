4a6b0e81c8ce18c648eed9b1d82d7b0359f01fdd
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-03 19:54:19 -0700

    1-init_dog 2

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include "main.h"
 
-struct dog{
-    char *name;
-    float age;
-    char *owner;
+/**
+*init_dog - function to add information about our pet.
+*@d: pointer.
+*@name: name.
+*@age: age.
+*@owner: owner.
+*Return: zero on success.
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner){
-    d->name = strdup(name);
-    d->age = age;
-    d->owner = strdup(owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
