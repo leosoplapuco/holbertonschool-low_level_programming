61b7ee0331c2dcabe7d4a71999469d9f744d1454
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-04 10:54:12 -0700

    dog 10

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,24 +1,19 @@
 #include <stdio.h>
 #include "main.h"
-
 #ifndef HEADER_FILE
 #define HEADER_FILE dog.h
+
 /**
- * struct dog - Define a new struct dog with name, age, owner
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of dog
- * Return: zero on success.
- **/
+* struct dog - Define a new struct dog with name, age, owner
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of dog
+* Return: zero on success.
+*/
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-typedef struct dog dog_t;
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif
