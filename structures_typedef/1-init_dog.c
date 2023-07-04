c3c82f36c71c2972990db6de0bb2186b4c8db351
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-04 13:27:50 -0700

    1-init_dog 11

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* init_dog - principal function.
+* @d: pointer of the elements.
+* @name: name pointer.
+* @age: age pointer.
+* @owner: owner pointer.
+* Return: zero on success.
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
