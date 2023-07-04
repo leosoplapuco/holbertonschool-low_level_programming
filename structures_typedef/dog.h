6275b379524596b6022cfa04c68a89d7f720d611
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-04 13:14:25 -0700

    dog 24

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
-/**
-* struct dog - adding information about our pet.
-*@name: name.
-*@age: age.
-*@owner: owner.
-*Return: zero on success.
-*/
-
 struct dog
 {
 	char *name;
