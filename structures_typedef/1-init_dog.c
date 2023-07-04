f6b19f12938905cf526769c392c8a1e119143df5
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-03 20:06:21 -0700

    1-init_dog 8

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -12,15 +12,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 
 int main(void) {
     struct dog my_dog;
-
-    init_dog(&my_dog, "Ghost", 4.75, "Jon Snow");
-    printf("My name is %s, and I am %.2f years old - Woof!\n", my_dog.name, my_dog.age);
-
-    // Liberar la memoria asignada
-    free(my_dog.name);
-    free(my_dog.owner);
-
-    return 0;
 }
 
 void init_dog(struct dog *d, char *name, float age, char *owner) {
