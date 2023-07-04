d77b12828a2cc8578315bc671b4e239109d5fe1b
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-04 13:44:35 -0700

    1-init_dog 17

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,5 +1,3 @@
-#include "main.h"
-
 int _putchar(char c);
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -10,5 +8,5 @@ int main(void)
     init_dog(&my_dog, "Bruno", 3.5, "Bob");
     printf("My name is %s, and I am %.1f years old - Woof!\n", my_dog.name, my_dog.age);
 
-    return 0;
+    return (0);
 }
