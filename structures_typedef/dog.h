f92df883ca9ecfda9ecaba7eaa83d13e38b3be89
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-03 17:10:39 -0700

    dog 2

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,1 +1,17 @@
 #include <stdio.h>
+
+/**
+*main - principal function to save infromation about our pets.
+*Return: zero on success.
+*/
+
+struct dog{
+    char *name;
+    float age;
+    char *owner;
+};
+
+int main(){
+    struct dog dog_0;
+    return (0);
+}
