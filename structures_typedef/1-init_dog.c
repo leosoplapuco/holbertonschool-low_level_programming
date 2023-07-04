c53e3439c0bcf313534f2f8d1a3f7fd71ec0a5a1
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-04 15:36:17 -0700

    1-init_dog 22

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -10,13 +10,6 @@
 * @owner: owner pointer.
 */
 
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
     if (d != NULL)
--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,2 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
-int _putchar(char c);
+
+#endif
