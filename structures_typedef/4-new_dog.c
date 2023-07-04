a82745f5da8ba4e87db42c12049d57e3493d80cf
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-04 16:02:46 -0700

    5-free_dog 2

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -37,27 +37,3 @@ dog_t *new_dog(char *name, float age, char *owner) {
 
     return new_dog;
 }
-
-int main(void) {
-    char *name = "Buddy";
-    float age = 3.5;
-    char *owner = "John";
-
-    dog_t *my_dog = new_dog(name, age, owner);
-    if (my_dog == NULL) {
-        printf("Failed to create a new dog.\n");
-        return 1;
-    }
-
-    printf("New dog created:\n");
-    printf("Name: %s\n", my_dog->name);
-    printf("Age: %.1f\n", my_dog->age);
-    printf("Owner: %s\n", my_dog->owner);
-
-    free(my_dog->name);
-    free(my_dog->owner);
-    free(my_dog);
-
-    return 0;
-}
-
