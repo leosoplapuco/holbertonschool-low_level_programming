7da15dd9a391d8a73d3dfdbe46fb323272b7e311
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-04 15:08:41 -0700

    1-init_dog 18

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -3,16 +3,18 @@
 
 /**
 * init_dog - principal function.
-* @d: pointer of the elements.
-* @name: name pointer.
-* @age: age pointer.
-* @owner: owner pointer.
-* Return: zero on success.
+* @d: name of struct
+* @name: name of dog in struct
+* @age: age of dog in struct
+* @owner: name of owner of dog in struct
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,12 +1,3 @@
 int _putchar(char c);
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-int main(void)
-{
-    struct dog my_dog;
-    init_dog(&my_dog, "Bruno", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f years old - Woof!\n", my_dog.name, my_dog.age);
-
-    return (0);
-}
+void init_dog(struct dog *d, char *name, float age, char *owner
