a3b2e897222d67208072dc7b709339af0753e149
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-05 12:12:34 -0700

    5-free_dog 14

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -8,19 +8,6 @@ struct dog
 	char *owner;
 };
 
-int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
-int main(void)
-{
-    dog_t *my_dog;
-
-    my_dog = new_dog("asdf", 3.5, "Jay");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-    free_dog(my_dog);
-    return (0);
-}
 #endif
