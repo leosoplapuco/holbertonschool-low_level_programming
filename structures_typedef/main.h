e76210ef1da6422ad967820b2de5b837221f63fa
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-05 12:37:24 -0700

    5-free_dog 16

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void free_dog(dog_t *d);
