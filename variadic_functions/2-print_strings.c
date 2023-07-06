0f7a41d1917931d74a8a2e4c653e91bfe3bf241f
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-06 11:26:39 -0700

    2-print_strings 5

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -5,52 +5,59 @@
 * print_strings - principal function.
 * @separator: space.
 * @n: initial value
-* Return: zero on success
+* Return: zero on success.
 */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
-	unsigned int i;
-	char *str;
+    va_list args;
+    unsigned int i;
+    char *str;
 
-	va_start(args, n);
+    va_start(args, n);
 
-	if (n > 0)
-	{
-		str = va_arg(args, char *);
+    if (n > 0)
+    {
+        str = va_arg(args, char *);
 
-		if (str == NULL)
-	{
-		printf("(nil)");
-	}
-	else
-	{
-		printf("%s", str);
-	}
+        if (str == NULL)
+        {
+            printf("(nil)");
+        }
+        else
+        {
+            printf("%s", str);
+        }
 
-	i = 1;
+        i = 1;
+        while (i < n)
+        {
+            str = va_arg(args, char *);
 
-	while (i < n)
-	{
-		str = va_arg(args, char *);
+            if (separator != NULL)
+            {
+                printf("%s", separator);
+            }
 
-		if (separator != NULL)
-		{
-			printf("%s", separator);
-		}
+            if (str == NULL)
+            {
+                printf("(nil)");
+            }
+            else
+            {
+                printf("%s", str);
+            }
 
-		if (str == NULL)
-		{
-			printf("(nil)");
-		}
-		else
-		{
-			printf("%s", str);
-		}
-		i++;
-	}
-}
-va_end(args);
-printf("\n");
+            i++;
+
+            if (i == n)
+            {
+                break;
+            }
+        }
+    }
+
+    va_end(args);
+
+    printf("\n");
 }
