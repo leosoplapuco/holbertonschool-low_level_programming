9727df43e2172239a3eea9eca5d156ed0a634d23
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-06 10:23:33 -0700

    0-sum_them_all 6

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,22 +3,22 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-    unsigned int i;
-    int sum = 0;
-    va_list args;
+	unsigned int i;
+	int sum = 0;
+	va_list args;
 
-    if (n == 0)
-        return 0;
+	if (n == 0)
+	{
+		return (0);
+	}
 
-    va_start(args, n);
+	va_start(args, n);
 
-    for (i = 0; i < n; i++)
-    {
-        int num = va_arg(args, int);
-        sum += num;
-    }
-
-    va_end(args);
-
-    return sum;
+	for (i = 0; i < n; i++)
+	{
+		int num = va_arg(args, int);
+		sum += num;
+	}
+	va_end(args);
+	return (sum);
 }
