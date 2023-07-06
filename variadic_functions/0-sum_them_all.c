4ba22219aa20267af18a9a844af3193111491dbc
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-06 10:13:01 -0700

    0-sum_them_all

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+#include "main.h"
+
+int sum_them_all(const unsigned int n, ...) {
+    if (n == 0)
+        return 0;
+
+    int sum = 0;
+
+    va_list args;
+    va_start(args, n);
+
+    for (unsigned int i = 0; i < n; i++) {
+        int num = va_arg(args, int);
+        sum += num;
+    }
+
+    va_end(args);
+
+    return sum;
+}
---
+++ variadic_functions/README.md
@@ -0,0 +1,1 @@
+Learning about the varidic function to be our code more dinamic.
