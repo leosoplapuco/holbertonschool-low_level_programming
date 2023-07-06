1a50a6956b4bddc592e88c4218234d1eaf23f655
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-06 10:21:18 -0700

    0-sum_them_all 5

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,20 +1,21 @@
 #include <stdarg.h>
 #include "main.h"
 
-int sum_them_all(const unsigned int n, ...) {
+int sum_them_all(const unsigned int n, ...)
+{
+    unsigned int i;
+    int sum = 0;
+    va_list args;
+
     if (n == 0)
         return 0;
 
-    int sum = 0;
-    unsigned int count = n;
-
-    va_list args;
     va_start(args, n);
 
-    while (count > 0) {
+    for (i = 0; i < n; i++)
+    {
         int num = va_arg(args, int);
         sum += num;
-        count--;
     }
 
     va_end(args);
