fa6ea956097faf212efea865bff4a407aeef4b38
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-06 10:34:15 -0700

    1-print_numbers 3

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -10,23 +10,21 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
+	va_list args;
+	unsigned int i;
 
-    va_start(args, n);
+	va_start(args, n);
 
-    for (i = 0; i < n; i++)
-    {
-        int num = va_arg(args, int);
-        printf("%d", num);
+	for (i = 0; i < n; i++)
+	{
+		int num = va_arg(args, int);
 
-        if (i < n - 1 && separator != NULL)
-        {
-            printf("%s", separator);
-        }
-    }
-
-    va_end(args);
-
-    printf("\n");
+		printf("%d", num);
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
 }
