787e056c1eb6d49f398c29d199a78e21699a20d3
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-07 16:12:33 -0700

    1-array_iterator 2

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+* array_iterator - principal function.
+* @array: value.
+* @size: value.
+* @action: value.
+* Return: zero on succes.
+*/
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
