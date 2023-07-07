18af53d3dda69b59e1d303258836308438ac5e29
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-07 16:16:19 -0700

    2-int_index 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 
+/**
+* int_index - principal function.
+* Return: zero on succes.
+*/
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
