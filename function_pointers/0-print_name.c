3e8dd12c6ebcb56cf6908192e57295766f50ab54
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-07 16:08:29 -0700

    0-print_name 2

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,6 +2,13 @@
 #include "function_pointers.h"
 #include <stdio.h>
 
+/**
+* print_name - principal function.
+* @name: char.
+* @f: value.
+* Return: zero on succes.
+*/
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && *name != '\0' && f != NULL)
