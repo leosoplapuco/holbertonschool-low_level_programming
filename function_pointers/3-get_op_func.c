c1937acdbdef28da0b09aec79cfa09f1b9809824
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-07 16:36:36 -0700

    3-main 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -3,22 +3,22 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
-    while (ops[i].op != NULL)
-    {
-        if (*(ops[i].op) == *s)
-            return (ops[i].f);
-        i++;
-    }
+	int i = 0;
 
-    return (NULL);
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
