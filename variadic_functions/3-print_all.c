79330b32b6949231df05dbe62690268a35bc3eda
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-07 17:11:13 -0700

    3-print_all 17

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -5,42 +5,38 @@ void print_all(const char * const format, ...)
 {
     va_list args;
     const char *ptr = format;
-    char c;
-    int i;
-    float f;
     char *s;
-    int count = 0;
+    int has_comma = 0;
 
     va_start(args, format);
 
     while (*ptr)
     {
-        if (count > 0)
+        if (has_comma)
             printf(", ");
+
+        has_comma = 1;
 
         switch (*ptr)
         {
             case 'c':
-                c = va_arg(args, int);
-                printf("%c", c);
+                printf("%c", va_arg(args, int));
                 break;
             case 'i':
-                i = va_arg(args, int);
-                printf("%d", i);
+                printf("%d", va_arg(args, int));
                 break;
             case 'f':
-                f = va_arg(args, double);
-                printf("%f", f);
+                printf("%f", va_arg(args, double));
                 break;
             case 's':
                 s = va_arg(args, char *);
                 printf("%s", (s == NULL) ? "(nil)" : s);
                 break;
             default:
+                has_comma = 0;
                 break;
         }
 
-        count++;
         ptr++;
     }
 
@@ -48,4 +44,3 @@ void print_all(const char * const format, ...)
 
     va_end(args);
 }
-
