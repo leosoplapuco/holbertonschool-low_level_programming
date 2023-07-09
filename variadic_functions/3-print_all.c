10aaf1915c5123448799ff6080090880c5aeda89
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-07-08 23:13:15 -0700

    3-print_all 20

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,56 +1,83 @@
 #include <stdio.h>
-#include <stdarg.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+/**
+ * print_i - prints int
+ * @list: arguement of list
+ * @s: seperator
+ * Return: none
+ */
+void print_i(va_list list, char *s)
+{
+	printf("%s%d", s, va_arg(list, int));
+}
+/**
+ * print_c - prints char
+ * @list: arguement char
+ * @sep: seperator
+ */
+void print_c(va_list list, char *sep)
+{
+	printf("%s%c", sep, va_arg(list, int));
+}
+/**
+ * print_s - prints string
+ * @sep: seperator
+ * @list: list to print
+ * Return: none
+ */
+void print_s(va_list list, char *sep)
+{
+	char *s;
 
+	s = va_arg(list, char *);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s%s", sep, s);
+}
+/**
+ * print_f - prints floats
+ * @sep: float to print
+ * @list: next arguement of list to print
+ * Return: none
+ */
+void print_f(va_list list, char *sep)
+{
+	printf("%s%f", sep, va_arg(list, double));
+}
+/**
+ * print_all - prints out all stuff
+ * @format: format is list of types of arguements
+ */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    const char *ptr = format;
-    char *s;
-    int has_comma = 0;
-
-    va_start(args, format);
-
-    while (*ptr)
-    {
-        char type = *ptr;
-
-        while (type != 'c' && type != 'i' && type != 'f' && type != 's')
-        {
-            ptr++;
-            type = *ptr;
-        }
-
-        switch (type)
-        {
-            case 'c':
-                printf("%c", va_arg(args, int));
-                break;
-            case 'i':
-                if (has_comma)
-                    printf(", ");
-                printf("%d", va_arg(args, int));
-                has_comma = 1;
-                break;
-            case 'f':
-                if (has_comma)
-                    printf(", ");
-                printf("%f", va_arg(args, double));
-                has_comma = 1;
-                break;
-            case 's':
-                if (has_comma)
-                    printf(", ");
-                s = va_arg(args, char *);
-                printf("%s", (s != NULL) ? s : "(nil)");
-                has_comma = 1;
-                break;
-        }
-
-        ptr++;
-    }
-
-    printf("\n");
-
-    va_end(args);
+	va_list list;
+	char *sep;
+	int i, j;
+	fm_t fm[] = {
+		{"c", print_c},
+		{"i", print_i},
+		{"f", print_f},
+		{"s", print_s},
+		{NULL, NULL}
+	};
+	va_start(list, format);
+	i = 0;
+	sep = "";
+	while (format != NULL && format[i] != '\0')
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == *(fm[j]).fm)
+			{
+				fm[j].p(list, sep);
+				sep = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(list);
 }
-
